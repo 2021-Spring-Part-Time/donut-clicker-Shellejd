802f3fc181b20234b8241fe732c925b067e38f7e
Shellejd <rochelle.dishmon@gmail.com>
2021-07-09 22:40:51 -0400

    donut maker project

--- index.js
+++ index.js
@@ -1,12 +1,43 @@
-import DonutMaker from "./DonutMaker.js";
+import DonutMaker from "./DonutMaker";
 
-const donutMaker = new DonutMaker(0, 0, 0);
+let sprinkleDonutMaker = new DonutMaker(0, 0, 100, 0, 10);
 
-const addBtn = document.querySelector("#AddDonut"),
-  donutCount = document.querySelector("#DonutCount");
+console.log(sprinkleDonutMaker);
 
-addBtn.addEventListener('click', () => {
-  donutMaker.addDonut();
-  donutCount.innerText = donutMaker.numDonuts;
+let addDonutBtn = (document.getElementById('addDonutBtn'));
+let addMultiplierBtn = (document.getElementById('addMultiplierBtn'));
+addMultiplierBtn.addEventListener('click', () => {
+  sprinkleDonutMaker.addDonut();
+  console.log(sprinkleDonutMaker.numDonuts);
+  document.getElementById('donutCount').innerHTML = sprinkleDonutMaker.numDonuts;
+});
 
-})
+addMultiplierBtn.addEventListener('click', () => {
+  conseloe.log(sprinkleDonutMaker.numMultipliers);
+  sprinkleDonutMaker.addMultipliers();
+
+  document.getElementById('donutCount').innerHTML = sprinkleDonutMaker.numDonuts;
+  document.getElementById('multiplier').innerHTML = sprinkleDonutMaker.numMultipliers;
+  document.getElementById('multiplierCost').innerHTML = sprinkleDonutMaker.multiplierCost;
+  console.log(sprinkleDonutMaker.numMultipliers);
+});
+
+let addAutoClickerBtn = (document.getElementById('addAutoClickerBtn'));
+addAutoClickerBtn.addEventListener('click', () => {
+  sprinkleDonutMaker.addAutoClicker();
+  sprinkleDonutMaker.activateAutoClickers();
+  console.log(sprinkleDonutMaker.numAutoClickers);
+  document.getElementById('autoClickersCost').innerHTML = sprinkleDonutMaker.autoClickersCost;
+  document.getElementById('autoClickers').innerHTML = sprinkleDonutMaker.numAutoClickers;
+  document.getElementById('donutCount').innerHTML = sprinkleDonutMaker.numDonuts;
+  document.getElementById('multiplierCost').innerHTML = sprinkleDonutMaker.multiplierCost;
+
+});
+
+setInterval(function () {
+  sprinkleDonutMaker.activateAutoClickers();
+  document.getElementById('donutsCount').innerHTML = sprinkleDonutMaker.numDonuts;
+}, 1000);
+
+
+//});
